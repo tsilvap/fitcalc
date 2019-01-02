f88556387c2ec96ddc1095dc3e185854f9c8b560
Thiago da Silva <thiagodasilva@protonmail.com>
2019-01-02 17:34:37 -0200

    FoodDropdown is glitched, so I implement my own

--- src/components/FoodDropdown.js
+++ src/components/FoodDropdown.js
@@ -1,39 +1,80 @@
 import React, { Component } from 'react';
-import { Form } from 'semantic-ui-react';
+import { Form, Input, List, Segment } from 'semantic-ui-react';
 
 import foodList from '../data/foodList.json';
-
-const options = foodList.map(food => {
-  const option = {
-    key: food.id,
-    content: food.name,
-    description: food.prepMethod,
-    value: food.id,
-    text: (
-      <div>
-        {food.name}
-        <span className="description">{food.prepMethod}</span>
-      </div>
-    )
-  };
-
-  return option;
-});
+import './FoodDropdown.css';
 
 class FoodDropdown extends Component {
+  state = { foodItems: [], searchQuery: '', selected: false, value: '' };
+
+  handleChange = e => {
+    const searchQuery = e.target.value;
+    this.setState({ searchQuery, selected: false, value: searchQuery });
+    this.updateFoodItems(searchQuery);
+  };
+
+  handleClick = e => {
+    // This looks convoluted. Is there a better way to implement this?
+    const foodId = e.currentTarget.attributes['data-value'].value;
+
+    // Set value of the input field
+    const foodName = e.currentTarget.firstChild.innerHTML;
+    const prepMethod = e.currentTarget.lastChild.textContent;
+    this.setState({ selected: true, value: `${foodName}, ${prepMethod}` });
+
+    // Call parent event handler
+    this.props.handleSelect(foodId);
+  };
+
+  updateFoodItems = query => {
+    if (query === '') {
+      this.setState({ foodItems: [] });
+      return;
+    }
+
+    const foodItems = foodList
+      .filter(food => {
+        const foodTerm = `${food.name}, ${food.prepMethod}`;
+        return foodTerm.toLowerCase().startsWith(query.toLowerCase());
+      })
+      .map(food => {
+        return (
+          <List.Item key={food.id} onClick={this.handleClick} value={food.id}>
+            <List.Header>{food.name}</List.Header>
+            {food.prepMethod}
+          </List.Item>
+        );
+      });
+
+    this.setState({ foodItems });
+    return;
+  };
+
   render() {
+    const { foodItems, searchQuery, selected } = this.state;
+
     return (
       <div className="food-dropdown">
-        <Form.Dropdown
-          deburr
-          fluid
-          label="Alimento"
-          onChange={(e, { value }) => this.handleFoodChange(value)}
-          options={options.slice(0, 10)}
-          placeholder="Nome do alimento"
-          search
-          selection
-        />
+        <Form.Field>
+          <label>Alimento</label>
+          <Input
+            icon="search"
+            iconPosition="left"
+            onChange={this.handleChange}
+            placeholder="Nome do alimento"
+            value={this.state.value}
+          />
+        </Form.Field>
+        <Segment
+          className="food-suggestions"
+          style={
+            searchQuery === '' || selected
+              ? { visibility: 'hidden' }
+              : { visibility: 'visible' }
+          }
+        >
+          <List selection>{foodItems.slice(0, 10)}</List>
+        </Segment>
       </div>
     );
   }
