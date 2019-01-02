52d3d3434f4e65eea7c394c349be47fb53378445
Thiago da Silva <thiagodasilva@protonmail.com>
2019-01-02 01:15:09 -0200

    Implement functionality in FoodForm

--- src/components/FoodForm.js
+++ src/components/FoodForm.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Button, Input, Form } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { Button, Form, Input, Popup } from 'semantic-ui-react';
 
 import './FoodForm.css';
 import foodList from '../data/foodList.json';
+import { addFood } from '../actions';
 
 const options = foodList.map(food => {
   const option = {
     key: food.id,
     content: food.name,
     description: food.prepMethod,
-    value: `${food.name} ${food.prepMethod}`,
+    value: food.id,
     text: (
       <div>
         {food.name}
@@ -21,43 +23,84 @@ const options = foodList.map(food => {
   return option;
 });
 
-// FoodForm Component.
-const FoodForm = () => {
-  return (
-    <div className="food-form">
-      <Form>
-        <Form.Group>
-          <div className="form-content">
-            <div className="form-select">
-              <Form.Select
-                deburr
-                fluid
-                label="Alimento"
-                options={options.slice(0, 10)}
-                placeholder="Nome do alimento"
-                search
-              />
-            </div>
-            <Form.Field>
-              <label>Quantidade</label>
-              <Input
-                label="gramas (g)"
-                labelPosition="right"
-                placeholder="Qtd."
-              />
-            </Form.Field>
-            <div className="form-button">
-              <Form.Field
-                control={Button}
-                content="Adicionar"
-                label="Adicionar"
-              />
-            </div>
-          </div>
-        </Form.Group>
-      </Form>
-    </div>
-  );
-};
+class FoodForm extends React.Component {
+  state = { popupIsOpen: false };
 
-export default FoodForm;
+  foodSelected = { foodId: null, quantity: null };
+
+  handleFoodChange = foodId => {
+    this.foodSelected.foodId = foodId;
+  };
+
+  handleQuantityChange = quantity => {
+    this.foodSelected.quantity = quantity;
+  };
+
+  handleSubmit = () => {
+    if (!this.foodSelected.foodId || !this.foodSelected.quantity) {
+      this.setState({ popupIsOpen: true });
+      setTimeout(() => {
+        this.setState({ popupIsOpen: false });
+      }, 3000);
+    } else {
+      this.props.addFood(
+        foodList.filter(food => food.id === this.foodSelected.foodId)[0],
+        this.foodSelected.quantity
+      );
+    }
+  };
+
+  render() {
+    return (
+      <div className="food-form">
+        <Form>
+          <Form.Group>
+            <div className="form-content">
+              <div className="form-select">
+                <Form.Select
+                  deburr
+                  fluid
+                  label="Alimento"
+                  onChange={(e, { value }) => this.handleFoodChange(value)}
+                  options={options.slice(0, 10)}
+                  placeholder="Nome do alimento"
+                  search
+                />
+              </div>
+              <Form.Field>
+                <label>Quantidade</label>
+                <Input
+                  label="gramas (g)"
+                  labelPosition="right"
+                  onChange={e => this.handleQuantityChange(e.target.value)}
+                  placeholder="Qtd."
+                />
+              </Form.Field>
+              <div className="form-button">
+                <Form.Field>
+                  <label>Adicionar</label>
+                  <Popup
+                    content="Preencha o nome do alimento e a quantidade consumida."
+                    label="Adicionar"
+                    on="click"
+                    onOpen={this.handleSubmit}
+                    open={this.state.popupIsOpen}
+                    position="top right"
+                    trigger={<Button content="Adicionar" />}
+                  />
+                </Form.Field>
+              </div>
+            </div>
+          </Form.Group>
+        </Form>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  {
+    addFood
+  }
+)(FoodForm);
