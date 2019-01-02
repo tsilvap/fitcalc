139dd41ba5fb6a7b94c49d35772526ad30b16f4b
Thiago da Silva <thiagodasilva@protonmail.com>
2019-01-01 22:02:48 -0200

    Fix dropdown onSelect text

--- src/components/FoodForm.js
+++ src/components/FoodForm.js
@@ -4,19 +4,24 @@ import { Button, Dropdown, Form } from 'semantic-ui-react';
 import './FoodForm.css';
 import foodList from '../data/foodList.json';
 
-// TODO: Find out why it is lagging, and fix it.
 const options = foodList.map(food => {
   const option = {
     key: food.id,
     content: food.name,
     description: food.prepMethod,
-    value: `${food.name} ${food.prepMethod}`
+    value: `${food.name} ${food.prepMethod}`,
+    text: (
+      <div>
+        {food.name}
+        <span className="description">{food.prepMethod}</span>
+      </div>
+    )
   };
 
   return option;
 });
 
-// FoodForm component
+// FoodForm Component.
 const FoodForm = () => {
   return (
     <div className="food-form">
