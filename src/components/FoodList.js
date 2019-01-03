5805e12e10acd32644fae0b20ebeee774d8f724c
Thiago da Silva <thiagodasilva@protonmail.com>
2019-01-02 22:37:35 -0200

    Improve FoodList layout

--- src/components/FoodList.js
+++ src/components/FoodList.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Card } from 'semantic-ui-react';
+import { Icon, Segment } from 'semantic-ui-react';
+
+import './FoodList.css';
 
 class FoodList extends Component {
   renderList = foodsConsumed => {
     const foodItems = foodsConsumed.map(({ food, quantity }) => {
       return (
-        <Card fluid key={food.id}>
-          <Card.Content>
-            <Card.Header>{food.name}</Card.Header>
-            <Card.Description>{food.prepMethod}</Card.Description>
-          </Card.Content>
-        </Card>
+        <Segment textAlign="left">
+          <Icon name="close" style={{ margin: '0 1em 0 0' }} />
+          <strong>{food.name}</strong>
+          <span className="prep-method">{food.prepMethod}</span>
+        </Segment>
       );
     });
 
@@ -21,11 +22,7 @@ class FoodList extends Component {
   render() {
     const foodsConsumed = this.props.foodsConsumed;
 
-    return (
-      <div className="food-list">
-        <Card.Group>{this.renderList(foodsConsumed)}</Card.Group>
-      </div>
-    );
+    return <div className="food-list">{this.renderList(foodsConsumed)}</div>;
   }
 }
 
